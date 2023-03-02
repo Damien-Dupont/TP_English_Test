a1b1fe8be74bc1a06dbc69a834d4463c3b046bd5
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2023-03-02 21:59:58 +0100

    english test init

---
+++ src/english_verbs/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Player
+
+class SignInForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = ['email', 'lastname', 'firstname', 'password', 'town']
+        widgets = {'password': forms.PasswordInput()}
+
+class ConjugaisonForm(forms.Form):
+    preterit = forms.CharField(max_length=50, label="preterit")
+    past_participle = forms.CharField(max_length=50, label="past_participle")
---
+++ src/english_verbs/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.contrib import messages
+from .forms import SignInForm, ConjugaisonForm
+from .models import Player, IrregularVerb
+
+# Create your views here.
+def sign_in(request):
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            player = form.save(commit=False)
+            player.joined_date = timezone.now()
+            player.save()
+            return redirect('index')
+    else:
+        form = SignInForm()
+    return render(request, 'english_verbs/signIn.html', {'form': form})
+
+def log_in(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('game')
+        else:
+            return render(request, 'english_verbs/logIn.html', {'error': 'Invalid username or password.'})
+    else:
+        return render(request, 'english_verbs/logIn.html')
+
+@login_required
+def log_out(request):
+    logout(request)
+    return redirect('index')
+
+@login_required
+def game(request):
+    return render(request, 'english_verbs/game.html')
+
+def play(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    
+    irregular_verb = IrregularVerb.objects.order_by('?').first()
+
+    if Player.objects.filter(email=request.user, irregular_verb=irregular_verb).exists():
+      messages.warning(request, "Vous avez déjà conjugué ce verbe.")
+
+    form = ConjugaisonForm()
+
+    return render(request, 'english_verbs/play.html', {'irregular_verb': irregular_verb, 'form': form})
+
+def end(request, result):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    player = Player.objects.get(user=request.user, irregular_verb=result['irregular_verb'])
+    player.results = result['results']
+    player.date_played = timezone.now()
+    player.save()
+
+    return render(request, 'english_verbs/end.html', {'result': result})
