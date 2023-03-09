f699e8e60d01d0d0b99583b53dcd254f69b14434
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2023-03-09 22:57:51 +0100

    added urls

--- english_verbs/english_verbs/urls.py
+++ english_verbs/english_verbs/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('signup/', views.sign_up, name='signup'),
+    path('login/', views.log_in, name='login'),
+    path('logout/', views.log_out, name='logout'),
+    path('game/', views.game, name='game'),
+    path('play/', views.play, name='play'),
+    path('end/', views.end, name='end'),
 ]
